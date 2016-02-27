62d0b3c92cb72a7e1c75e06b0f41a73f834dc4e9
VasAndr <vasiliev_as@mail.ru>
2016-02-27 20:51:24 +0300

    Добавлен овый запускаемый класс DistPtP

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProga.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProga.java
@@ -4,12 +4,10 @@ public class MyFirstProga {
    public static void main(String[] args) {
        System.out.println("Hello World!");
 
-       Point p1 = new Point();
-       p1.x = 2;
-       p1.y = 6;
-       Point p2 = new Point();
-       p2.x = 5;
-       p2.y = 2;
+       Point p1 = new Point(2, 6);
+
+       Point p2 = new Point(5, 2);
+
        System.out.println("Расстояние между двумя точками (" +
                p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") = " + distance(p1, p2));
 
