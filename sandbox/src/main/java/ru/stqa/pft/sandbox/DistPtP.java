bb14fb125e5d86f773501658d4b5793da3d581c4
VasAndr <vasiliev_as@mail.ru>
2016-02-28 01:24:16 +0300

    2 примера вычисления расстояния при помощи метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistPtP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistPtP.java
@@ -12,6 +12,11 @@ public class DistPtP {
         System.out.println("Расстояние между двумя точками (" +
                 p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") = " + p2.distance(p1));
 
+        p1 = new Point(-0.999, 5.001);
+        p2 = new Point(-0, 0);
+
+        System.out.println("Расстояние между двумя точками (" +
+                p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") = " + p1.distance(p2));
     }
     /*public static double distance(Point p1, Point p2) {
         return Math.sqrt(Math.pow(p2.x-p1.x,2)+Math.pow(p2.y-p1.y,2));
