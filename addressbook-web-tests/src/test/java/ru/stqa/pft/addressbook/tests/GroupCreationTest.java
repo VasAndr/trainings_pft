cd0f3ac9535c0dc42ef6708c2802d830f99a788e
VasAndr <vasiliev_as@mail.ru>
2016-03-08 14:33:56 +0300

    ApplicationManager, GroupHelper, NavigationHelper и SessionHelper были созданы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTest extends TestBase {
+
+    @Test
+    public void testGroupCreation() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("Ex_Group1", "Log_text", "Comment_text"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
+    }
+
+}
