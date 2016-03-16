20b8cffc7abe3d5ff8ec25ae8f3a553cd7734dc3
VasAndr <vasiliev_as@mail.ru>
2016-03-17 02:21:35 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -14,18 +14,32 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void returnToHomePage() {
+        if (isElementPresent(By.linkText("home page")) && isElementPresent(By.linkText("add next"))) {
+            return;
+        }
         click(By.linkText("home page"));
     }
 
     public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home"));
     }
 
     public void gotoContactPage() {
+        if (isElementPresent(By.name("submit"))) {
+            return;
+        }
         click(By.linkText("add new"));
     }
 }
