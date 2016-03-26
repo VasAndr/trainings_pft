90b9c7485802444a36bfa878cd9d07ef8f41b947
VasAndr <vasiliev_as@mail.ru>
2016-03-26 18:03:52 +0300

    Зделаны изменения модуля_6 для Group

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.List;
 
 /**
  * Created by Hermit on 13.03.2016.
@@ -14,10 +17,13 @@ public class GroupModificationTest extends TestBase{
      if (! app.getGroupHelper().isThereAGroup()) {
          app.getGroupHelper().createGroup(new GroupData("Ex_Group1", "Log_text", "Comment_text"));
      }
-     app.getGroupHelper().selectGroup();
+     List<GroupData> before = app.getGroupHelper().getGroupList();
+     app.getGroupHelper().selectGroup(before.size() - 1);
      app.getGroupHelper().initGroupModification();
      app.getGroupHelper().fillGroupForm(new GroupData("Mod_Group1", "Log_text", "New_Comment_text"));
      app.getGroupHelper().submitGroupModification();
      app.getGroupHelper().returnToGroupPage();
+  List<GroupData> after = app.getGroupHelper().getGroupList();
+  Assert.assertEquals(after.size(), before.size());
  }
 }
