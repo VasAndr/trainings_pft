7829c1cec40eb9596d158c4a23faa0d81dd39aae
VasAndr <vasiliev_as@mail.ru>
2016-04-24 01:13:18 +0300

    Нарезка строк и их сравнение (m10)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -109,10 +109,36 @@ public class ContactHelper extends HelperBase {
             List<WebElement> cells = row.findElements(By.tagName("td"));
             String lastname = cells.get(1).getText();
             String firstname = cells.get(2).getText();
+            String[] phones = cells.get(5).getText().split("\n");
             int id = Integer.parseInt(row.findElement(By.tagName("input")).getAttribute("id"));
-            contactCache.add(new ContactData().withId(id).withFirstName(firstname).withLastName(lastname));
+            contactCache.add(new ContactData().withId(id).withFirstName(firstname).withLastName(lastname)
+                    .withHomePhone(phones[0]).withMobile(phones[1]).withWorkPhone(phones[2]));
         }
         return new Contacts(contactCache);
     }
 
+    public ContactData infoFromEditForm(ContactData contact) {
+        initContactModificationById(contact.getId());
+        String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
+        String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
+        String home = wd.findElement(By.name("home")).getAttribute("value");
+        String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
+        String work = wd.findElement(By.name("work")).getAttribute("value");
+        wd.navigate().back();
+        return new ContactData().withId(contact.getId()).withFirstName(firstname).withLastName(lastname)
+                .withHomePhone(home).withMobile(mobile).withWorkPhone(work);
+    }
+
+    private void initContactModificationById(int id) {
+        wd. findElement(By.cssSelector(String.format("a[href='edit.php?id=%s']", id))).click();
+
+        // wd. findElement(By.xpath(String.format("//tr[.//input[@value='%s']]/td[8]/a", id))).click();
+        // wd. findElement(By.xpath(String.format("//input[@value='%s']/../../td[8]/a", id))).click();
+        /*
+        WebElement checkbox = wd.findElement(By.cssSelector(String.format("input[@value='%s']", id)));
+        WebElement row = wd.findElement(By.xpath("./../.."));
+        List<WebElement> cells = row.findElements(By.tagName("td"));
+        cells.get(7).findElement(By.tagName("a")).click();
+         */
+    }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
@@ -0,0 +1,42 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by Hermit on 23.04.2016.
+ */
+public class ContactPhoneTest extends TestBase {
+
+    @BeforeMethod
+    public void ensurePrecondition(){
+        app.goTo().homePage();
+        if (app.contact().all().size() == 0) {
+            app.contact().create(new ContactData().withFirstName("Andr").withMidName("Serg").withLastName("Vas")
+                    .withNick("chirp").withTitle("title").withCompany("PFT").withAddress("Dark st, 4//13-7")
+                    .withHomePhone("6765867867979").withMobile("23477894").withWorkPhone("08653423").withFax("4654769")
+                    .inGroup("Ex_Group1"));
+        }
+    }
+
+    @Test //(enabled = false)
+    public void testContactPhones () {
+        app.goTo().homePage();
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+        assertThat(contact.getHomePhone(), equalTo(cleaned(contactInfoFromEditForm.getHomePhone())));
+        assertThat(contact.getMobile(), equalTo(cleaned(contactInfoFromEditForm.getMobile())));
+        assertThat(contact.getWorkPhone(), equalTo(cleaned(contactInfoFromEditForm.getWorkPhone())));
+    }
+
+    public String cleaned(String phone) {
+        return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
+    }
+}
